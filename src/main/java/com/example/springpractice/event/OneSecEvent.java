505cb37ba7765d855a391cf8961daf89e2727f7c
azs-r-takata <r-takata@AZPC0175>
2025-08-06 09:07:34 +0900

    第６回

---
+++ src/main/java/com/example/springpractice/event/OneSecEvent.java
@@ -0,0 +1,9 @@
+package com.example.springpractice.event;
+
+import org.springframework.context.ApplicationEvent;
+
+public class OneSecEvent extends ApplicationEvent {
+	public OneSecEvent(Object source) {
+		super(source);
+	}
+}
---
+++ src/main/java/com/example/springpractice/security/UserDetailsImpl.java
@@ -0,0 +1,49 @@
+package com.example.springpractice.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.springpractice.entity.User;
+
+
+public class UserDetailsImpl implements UserDetails {
+	private final User user;
+	private final Collection<GrantedAuthority> authorities;
+	
+	public UserDetailsImpl(User user, Collection<GrantedAuthority> authorities) {
+		this.user = user;
+		this.authorities = authorities;
+	}
+	
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+	
+	@Override
+	public String getUsername() {
+		return user.getUserName();
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
