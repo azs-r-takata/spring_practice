d0e75166e0a34e6247837a6e594a63a3ac8ae3fc
azs-r-takata <r-takata@AZPC0175>
2025-08-19 16:19:06 +0900

    追加

--- src/main/java/com/example/springpractice/security/WebSecurityConfig.java
+++ src/main/java/com/example/springpractice/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 		    .authorizeHttpRequests((requests) -> requests
-		    		.requestMatchers("/login", "/resources/**").permitAll()
+		    		.requestMatchers("/login", "/**", "/resources/**").permitAll()
 		    		.anyRequest().authenticated()
 		    )
 		    .formLogin((form) -> form
